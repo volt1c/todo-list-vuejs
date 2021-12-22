ac52748216a4799d21a7126c0eb2e7277b26323b
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-12-22 23:59:02 +0100

    feat: set pwa colors

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   pwa: {
     name: 'TODO',
+    themeColor: '#000000',
+    msTileColor: '#FFFFFF',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
   },
