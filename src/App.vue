b958ad0f62441b3a08febc88efc6d3ec0acc48da
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-12-23 17:42:54 +0100

    feat: implement components... AddTask, GithubCorner

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
+    <github-corner href="https://github.com/volt1c/todo-list-vuejs" />
     <div class="container">
       <h1 class="title">Get Sh*t Done:</h1>
       <ul class="list">
         <task-item v-for="todo in todos" :key="todo.id" v-bind:task="todo" />
       </ul>
-      <div class="add-task">
-        <input class="add-task-input" type="text" placeholder="add sh*t to list" v-model="newTask" />
-        <button class="add-task-btn" type="button" v-on:click="addTodo()">
-          <plus-icon size="1.5x" />
-        </button>
-      </div>
+      <add-task v-on:reload="reloadTasks()" />
     </div>
-    <a class="fixed-link right-1" href="https://github.com/volt1c/todo-list-vuejs">
-      <github-icon size="1.75x" />
-    </a>
   </div>
 </template>
 
 <script>
-import TaskItem from './components/TaskItem.vue';
-import { GithubIcon } from 'vue-feather-icons';
-import { PlusIcon } from 'vue-feather-icons';
-import { getCookiesObject, setCookiesObject } from './assets/js/cookies';
+import GithubCorner from './components/GithubCorner';
+import TaskItem from './components/TaskItem';
+import AddTask from './components/AddTask';
+import { getCookiesObject } from './assets/js/cookies';
 
 export default {
   name: 'App',
@@ -31,27 +24,14 @@ export default {
     newTask: '',
   }),
   methods: {
-    addTodo() {
-      if (this.newTask.trim() == '')
-        return console.warn('> newTask is empty...');
-
-      const obj = getCookiesObject();
-      if (!obj.todos) obj.todos = [];
-      obj.todos.push({
-        id: Date.now(),
-        value: this.newTask.trim(),
-        isDone: false,
-      });
-      setCookiesObject(obj);
-
+    reloadTasks() {
       this.todos = getCookiesObject().todos;
-      this.newTask = '';
     },
   },
   components: {
     TaskItem,
-    GithubIcon,
-    PlusIcon,
+    GithubCorner,
+    AddTask,
   },
 };
 </script>
