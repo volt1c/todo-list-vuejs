368eb48230e677f5e4dbf705bab211abe02ce0b6
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2024-06-19 09:19:23 -0700

    Revert "refactor: use shorthands"
    This reverts commit a69bcc01d6c1182eb7df9729cef80f3f056d8899.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <h1 class="title">Get Sh*t Done:</h1>
     <ul class="list">
-      <task-item v-for="todo in todos" :key="todo.id" :task="todo" />
+      <task-item v-for="todo in todos" :key="todo.id" v-bind:task="todo" />
     </ul>
-    <add-task @reload="reloadTasks()" />
+    <add-task v-on:reload="reloadTasks()" />
     <github-corner href="https://github.com/volt1c/todo-list-vuejs" />
   </div>
 </template>
